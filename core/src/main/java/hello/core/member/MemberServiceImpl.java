1a6d01e538e2e7cc65d342ac76214dea3197c6ad
xi_jjun <rlawowns97@naver.com>
2022-06-07 00:29:56 +0900

    feat: add Member Respository,Service

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+	private final MemberRepository memberRepository = new MemoryMemberRepositoryImpl();
+
+	@Override
+	public void join(Member member) {
+		memberRepository.save(member);
+	}
+
+	@Override
+	public Member findMember(Long id) {
+		return memberRepository.findById(id);
+	}
+}
